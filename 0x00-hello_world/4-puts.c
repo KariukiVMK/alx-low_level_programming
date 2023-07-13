240fb3ea5c22454cb5dd99d126dd12382c70e2d6
KariukiVMK <ivmkariuki@gmail.com>
2023-07-13 07:12:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Starting. A program that prints a line with put func
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
